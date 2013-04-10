555618ffad9ea4fbcf29309da9e128a4fa5f23c3
toshi <toshi19.12@gmail.com>
2013-04-10 16:57:53 +0700

    Make a basic User model (including secure passwords)

---
+++ app/models/user.rb
@@ -0,0 +1,29 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class User < ActiveRecord::Base
+  attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
+  validates_confirmation_of :password
+
+
+  before_save { |user| user.email = email.downcase }
+  #presence: trueで値が入っているかどうかの確認。
+  #length: { maximum: 50 }で文字列の長さを設定。
+  #
+  validates_confirmation_of :password
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,presence: true, format: { with: VALID_EMAIL_REGEX },			 uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6  }
+  validates :password_confirmation, presence: true
+
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,9 +5,9 @@ describe "UserPages" do
   subject { page }
 
   describe "signup page" do
-	before { visit signup_path }
+	before { visit signin_path }
 
 	it { should have_selector('h1', text: 'Sign up')}
-	it { shlould have_selector('title', text: full_title('Sign up'))}
+	it { should have_selector('title', text: full_title('Sign up'))}
   end
 end
